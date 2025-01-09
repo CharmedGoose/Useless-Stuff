e816815223d8d9889e051c0fc9302cf96e24f72b
CharmedGoose <charmedgoose1@gmail.com>
2025-01-09 17:02:51 -0500

    init

---
+++ src/client/java/net/goosehub/useless/UselessStuffDataGenerator.java
@@ -0,0 +1,11 @@
+package net.goosehub.useless;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class UselessStuffDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
