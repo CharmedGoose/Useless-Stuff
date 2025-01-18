f142a72e8c55f34c5585bcd3090176c289a25784
CharmedGoose <charmedgoose1@gmail.com>
2025-01-17 21:36:21 -0500

    read and write nbt

--- src/main/java/net/goosehub/useless/block/entity/CompactorBlockEntity.java
+++ src/main/java/net/goosehub/useless/block/entity/CompactorBlockEntity.java
@@ -3,10 +3,14 @@ package net.goosehub.useless.block.entity;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.nbt.NbtList;
+import net.minecraft.registry.RegistryWrapper;
 import net.minecraft.util.math.BlockPos;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class CompactorBlockEntity extends BlockEntity {
 
@@ -33,5 +37,40 @@ public class CompactorBlockEntity extends BlockEntity {
     public void clearItems() {
         items = new ArrayList<>();
         markDirty();
+    }
+
+    @Override
+    protected void writeNbt(NbtCompound nbt, RegistryWrapper.WrapperLookup registryLookup) {
+
+        nbt.putInt("itemCount", items.size());
+
+        NbtList nbtList = new NbtList();
+        for (ItemStack item : items) {
+            nbtList.add(item.toNbt(registryLookup));
+        }
+
+        nbt.put("items", nbtList);
+
+        super.writeNbt(nbt, registryLookup);
+    }
+
+    @Override
+    protected void readNbt(NbtCompound nbt, RegistryWrapper.WrapperLookup registryLookup) {
+        items = new ArrayList<>();
+
+        int itemCount = nbt.getInt("itemCount");
+
+        NbtList nbtList = (NbtList) nbt.get("items");
+
+        if (nbtList != null) {
+            for (int i = 0; i < itemCount; i++) {
+                ItemStack itemStack = ItemStack.fromNbtOrEmpty(registryLookup, nbtList.getCompound(i));
+                if (!itemStack.isEmpty()) {
+                    items.add(itemStack);
+                }
+            }
+        }
+
+        super.readNbt(nbt, registryLookup);
     }
 }
